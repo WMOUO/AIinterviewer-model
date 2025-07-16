8a92dd1fe7cd25daa660f472449f1df9a9ff4e6d
wmouo <5316eictlws.2@gmail.com>
2025-07-16 17:16:26 +0800

    chore. 加入cors中間層

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import torch.nn.functional as F
 import soundfile as sf
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-from typing import List, Tuple
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://a-iinterviewer.5316eictlws-2.workers.dev/"],  # 可加上你的正式前端網址
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ImprovedFeatureExtractorLibrosa:
     def __init__(self,
